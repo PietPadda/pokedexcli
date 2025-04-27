eb2c9e2c6f4fe6954508a7b2c96488f353a06404
PietPadda <pietergdewaal@gmail.com>
2025-04-27 11:55:41 +0200

    add command registry, help and exit commands, and updated startREPL to actually use these commands. error handling also included

--- repl.go
+++ repl.go
@@ -8,10 +8,60 @@ import (
 	"strings" // for Fields (split whitespace) and ToLower (lowercase)
 )
 
-// we'll call this in main
+// our command registry (abstraction)
+// allows use to manage all commands we'll be adding
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// returns a map of all REPL commands we have
+// all commands are "registered" here
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": { // exit command
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": { // help command
+			name:        "help",
+			description: "List all Commands",
+			callback:    commandHelp,
+		},
+	}
+}
+
+// callback - terminates the program
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0) // neatly terminate program
+	return nil // no error on exit
+}
+
+// callback - lists all registered commands
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n") // newline at end for separation of command list
+
+	commands := getCommands() // get all commands
+
+	// loop thru all commands and print them
+	for _, command := range commands {
+		// print command name and description
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	// return success
+	return nil
+}
+
+// startREPL starts the Read-Eval-Print-Loop for the Pokedex CLI
 func startREPL() {
 	// block until user input
 	scanner := bufio.NewScanner(os.Stdin) // wait for input
+	commands := getCommands()             // get all commands
 
 	// infinite loop
 	for {
@@ -28,10 +78,23 @@ func startREPL() {
 			continue // don't do the rest
 		}
 
-		firstWord := cleanedInput[0] // get first word from input
+		commandInput := cleanedInput[0] // get first word from input
 
-		// print first word to CLI
-		fmt.Printf("Your command was: %s\n", firstWord)
+		// loop through command list to check if input exists (registry lookup)
+		command, ok := commands[commandInput] // see if input exists here
+
+		// if it exists callback it
+		if ok {
+			err := command.callback() // return callback err value to car
+
+			// callback check
+			if err != nil {
+				fmt.Println(err) // Errorf doesn't work here, we don't have error output
+			}
+		}
+
+		// otherwise it doesn't exist
+		fmt.Println("Invalid command")
 	}
 }
 
