b0d005b71f5fe384e0447c0a9afedbfd6fd8f053
PietPadda <pietergdewaal@gmail.com>
2025-04-27 11:19:51 +0200

    added a basic repl command to main, we now get text prompts and outputs as we type it... but it doesnt do anything yet

--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 // main.go
 package main // all files in same folder form part of package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello, World!")
+	// call start REPL to run the application
+	startREPL()
 }
--- repl.go
+++ repl.go
@@ -1,7 +1,39 @@
 // repl.go
 package main // all files in same folder form part of package main
 
-import "strings" // for Fields (split whitespace) and ToLower (lowercase)
+import (
+	"bufio"   // for input blocking
+	"fmt"     // for printing
+	"os"      // for OS input
+	"strings" // for Fields (split whitespace) and ToLower (lowercase)
+)
+
+// we'll call this in main
+func startREPL() {
+	// block until user input
+	scanner := bufio.NewScanner(os.Stdin) // wait for input
+
+	// infinite loop
+	for {
+		// print our CLI prompt
+		fmt.Print("Pokedex > ") // no newline
+
+		// get user input and clean
+		scanner.Scan()                        // read the user input
+		userInput := scanner.Text()           // get the user input
+		cleanedInput := cleanInput(userInput) // clean the input (lowercase,no WS, slice)
+
+		// empty input edge case handle
+		if len(cleanedInput) == 0 {
+			continue // don't do the rest
+		}
+
+		firstWord := cleanedInput[0] // get first word from input
+
+		// print first word to CLI
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
+}
 
 // make the words lower cases + plit words by whitespace (removes WS)
 func cleanInput(text string) []string {
